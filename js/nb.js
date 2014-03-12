c71700f212b542cb7150805bec51719b493bdd51
Tom Bates <tombagoo@gmail.com>
2014-03-11 22:43:17 -0700

    add Dropbox to authorize call

--- js/nb.js
+++ js/nb.js
@@ -39,7 +39,7 @@ nbx.open = function() {
 		}
     };
 
-	Nimbus.Auth.authorize();
+	Nimbus.Auth.authorize("Dropbox");
 
 
     ///nbx.Jots = Nimbus.Model.setup("Jots", ["descrip", "done", "id", "jot", "timestamp"]);
