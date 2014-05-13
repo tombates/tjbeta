26dd8d789f117915b6cbeb0ebc2dc3a7d95cc143
Tom Bates <tombagoo@gmail.com>
2014-05-13 11:42:21 -0700

    persist tag filtering

--- js/tjdb.js
+++ js/tjdb.js
@@ -1173,49 +1173,23 @@ function applyFilters() {
     //    || document.getElementById("filter_by_tags_and").checked || document.getElementById("filter_by_date").checked)) {
     if(!(document.getElementById("filter_by_date").checked || document.getElementById("filter_by_tags").checked)) {
         tj.indexedDB.showAllJots();
-        return;
-    }
-
-    // set tag filtering mode
-    // if(document.getElementById("filter_by_tags").checked) {
-    //     tj.filterObject.filterMode |= tj.FILTERMODE_TAGS;
-    //     // if(document.getElementById("filter_by_tags_or").checked) {
-    //     //     tj.filterObject.filterMode = tj.FILTERMODE_TAGS_OR;       
-    //     // }
-    //     // else if(document.getElementById("filter_by_tags_and").checked) {
-    //     //     tj.filterObject.filterMode = tj.FILTERMODE_TAGS_AND;       
-    //     // }
-    // }
-    // else {
-    //     tj.filterObject.filterMode &= ~(tj.FILTERMODE_TAGS);
-    //     //tj.filterObject.filterMode &= ~(tj.FILTERMODE_TAGS | tj.FILTERMODE_TAGS_OR | tj.FILTERMODE_TAGS_AND);
-    // }
-
-    // record radio buttons state separately so user can turn tag filter on/off while keeping or/and state
-    if(document.getElementById("filter_by_tags_or").checked) {
-        tj.filterObject.filterMode |= tj.FILTERMODE_TAGS_OR;       
     }
     else {
-        tj.filterObject.filterMode &= ~(tj.FILTERMODE_TAGS_OR);       
+        // record radio buttons state separately so user can turn tag filter on/off while keeping or/and state
+        if(document.getElementById("filter_by_tags_or").checked) {
+            tj.filterObject.filterMode |= tj.FILTERMODE_TAGS_OR;       
+        }
+        else {
+            tj.filterObject.filterMode &= ~(tj.FILTERMODE_TAGS_OR);       
+        }
+        if(document.getElementById("filter_by_tags_and").checked) {
+            tj.filterObject.filterMode |= tj.FILTERMODE_TAGS_AND;       
+        }
+        else {
+            tj.filterObject.filterMode &= ~(tj.FILTERMODE_TAGS_AND);       
+        }
+        tj.indexedDB.showAllJots(tj.filterObject);
     }
-    if(document.getElementById("filter_by_tags_and").checked) {
-        tj.filterObject.filterMode |= tj.FILTERMODE_TAGS_AND;       
-    }
-    else {
-        tj.filterObject.filterMode &= ~(tj.FILTERMODE_TAGS_AND);       
-    }
-
-
-    // set date filtering mode
-    // if(document.getElementById("filter_by_date").checked) {
-    //     //TODO put date info into filterObject
-    //     tj.filterObject.filterMode |= tj.FILTERMODE_DATE;        
-    // }
-    // else {
-    //     tj.filterObject.filterMode &= ~(tj.FILTERMODE_DATE);
-    // }
-
-    tj.indexedDB.showAllJots(tj.filterObject);
 
     // finally, persist the filter incase the user closes
     tj.indexedDB.persistFilterObjects();
