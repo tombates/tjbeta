7e045c28dda2c8886839a41406b01395507c613c
Tom Bates <tombagoo@gmail.com>
2014-07-21 15:39:58 -0700

    removing local jot storage

--- js/nb.js
+++ js/nb.js
@@ -222,14 +222,14 @@ function persistAuthorization() {
         };
 }
 
-function nimbus_init() {
-	console.log("doing NimbusBase nimbus_init()");
-    tj.indexedDB.open();
-    //indexedDB_init();
+// function nimbus_init() {
+// 	console.log("doing NimbusBase nimbus_init()");
+//     tj.indexedDB.open();
+//     //indexedDB_init();
+//
+// 	///nbx.open();  // connects to user storage using NimbusBase
+// }
 
-	///nbx.open();  // connects to user storage using NimbusBase
-}
-
-window.addEventListener("DOMContentLoaded", nimbus_init, false);
+window.addEventListener("DOMContentLoaded", tj.indexedDB.open, false);
 
 
