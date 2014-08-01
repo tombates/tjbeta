eaa2b65dbd1d1b23053947800d680bdb0771e7ba
Tom Bates <tombagoo@gmail.com>
2014-07-31 17:06:49 -0700

    default title

--- js/tjdb.js
+++ js/tjdb.js
@@ -276,7 +276,8 @@ tj.addJot = function() {
 */
 tj.getDefaultTitle = function(jotText) {
     var prefix = jotText.substring(0, tj.DEFAULT_TITLE_LIMIT);
-    var regexp = /^[^!?.\r\n]*[.!?\r\n]{1}/;
+    var firstline = prefix.split(/\r?\n/g);
+    var regexp = /^[^!?.]*[.!?]{1}/;
     var matching = prefix.match(regexp);
     if(matching === null)
         return prefix
